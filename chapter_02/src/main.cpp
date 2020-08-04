44414789f0798881c34804ac9c6c8b375f889fe6
Zdenko Pavlik Extern <zdenko.pavlik.extern@corpuls.com>
2020-08-04 11:54:44 +0200

    InProgress

---
+++ chapter_02/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Playground project for C++
+*/
+#define UNUSED(expr) (void)(expr) //clang-tidy
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
