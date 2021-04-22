1f9299ae1806981dcb60fdcb2fc850aac2c0a31b
zdenko.pavlik.extern <zdenko.pavlik.extern@corpuls.com>
2021-04-22 13:37:17 +0200

    templates, sfinae, renaming of folders

--- chapter_08_templates/src/sfinae.hpp
+++ chapter_08_templates/src/sfinae.hpp
@@ -1,10 +1,55 @@
 #pragma once
 
 #include <iostream>
+#include <cassert>
+#include <boost/assert.hpp>
 
 //https://www.bfilipek.com/2016/02/notes-on-c-sfinae.html
 //https://en.cppreference.com/w/cpp/language/sfinae
+//https://en.wikipedia.org/wiki/Substitution_failure_is_not_an_error
+
+struct Bar
+{
+    typedef int m_memberVariable;
+};
+
+template <typename T>
+typename T::m_memberVariable foo(const T& t)
+{
+    std::cout << "foo<T>" << std::endl;
+    return 0;
+}
+
+//template <typename T, typename std::enable_if<std::is_same<T, foundation::Bool>::value>::type* = nullptr> //only for foundation::Bool
+
+template <typename T,
+    std::enable_if_t<std::is_integral<T>::value, bool> = true>
+void compileableOnlyWithIntegers(T input)
+{
+    std::cout << "Input is " << input << std::endl;
+}
+
 void sfinaePlayground()
 {
-    std::cout << "-----------------------------------------------------------------" << std::endl;
+    try
+    {
+        std::cout << "-----------------------------------------------------------------" << std::endl;
+        foo(Bar());
+        //foo(0); // error, missing m_memberVariable
+        std::cout << "-----------------------------------------------------------------" << std::endl;
+        //BOOST_ASSERT_MSG(1 > 2, "1 is not bigger than 2");
+        //assert(1 > 2 && "1 is not bigger than 2");
+        std::cout << "-----------------------------------------------------------------" << std::endl;
+        compileableOnlyWithIntegers<int>(2);
+        //compileableOnlyWithIntegers<std::string>("2");
+        std::cout << "-----------------------------------------------------------------" << std::endl;
+    }
+    catch(std::exception& e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+    catch(...)
+    {
+        std::cout << "Exception..." << std::endl;
+    }
 }
