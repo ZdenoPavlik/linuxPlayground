effae7e7fdf04261a5347878a1816d771f0d20bf
zdenko.pavlik.extern <zdenko.pavlik.extern@corpuls.com>
2021-05-28 08:45:09 +0200

    Coroutines PoC

--- chapter_09_concepts/src/main.cpp
+++ chapter_09_concepts/src/main.cpp
@@ -5,12 +5,27 @@
 
 #include <iostream>
 
+/*template <class T>
+concept Incrementable = std::is_integral<T>::value;
+
+template <class T>
+concept Decrementable = std::is_integral<T>::value;
+
+template <typename T>
+requires Incrementable<T>&& Decrementable<T> void calculationFunction(T num1, T num2)
+{
+    std::cout << "Diff is " << (num1 - num2) << std::endl;
+    std::cout << "Sum is " << (num1 + num2) << std::endl;
+}*/
+
 int main(int argc, char** argv)
 {
     UNUSED(argc);
     UNUSED(argv);
 
     std::cout << "Hello World!" << std::endl;
+
+    //calculationFunction<int>(1, 5); // supported by at least gcc 10
 
     return 0;
 }
