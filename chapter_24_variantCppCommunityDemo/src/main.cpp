75b72267028f349f5d6a1d0d2562417dc6a33330
Zdeno Pavlik <zdeno.pavlik@gmail.com>
2021-07-12 15:00:17 +0200

    Std::variant, C++ coomunity demo

--- chapter_24_variantCppCommunityDemo/src/main.cpp
+++ chapter_24_variantCppCommunityDemo/src/main.cpp
@@ -13,7 +13,12 @@ class Clazz1
 public:
     void operator()() const
     {
-        std::cout << "Hello from clazz1" << std::endl;
+        std::cout << "Hello from clazz 1" << std::endl;
+    }
+
+    void hello1() const
+    {
+        std::cout << "Clazz 1: Hello" << std::endl;
     }
 };
 
@@ -22,7 +27,16 @@ class Clazz2
 public:
     void operator()() const
     {
-        std::cout << "Hello from clazz2" << std::endl;
+        std::cout << "Hello from clazz 2" << std::endl;
+    }
+};
+
+class Clazz3
+{
+public:
+    void hello() const
+    {
+        std::cout << "Hello from clazz 3" << std::endl;
     }
 };
 
@@ -30,11 +44,15 @@ struct DetermingVisitor
 {
     void operator()(const Clazz1& c1)
     {
-        c1();
+        c1.hello1();
     }
     void operator()(const Clazz2& c2)
     {
         c2();
+    }
+    void operator()(const Clazz3& c3)
+    {
+        c3.hello();
     }
     void operator()(const int i)
     {
@@ -56,50 +74,52 @@ int main()
     std::cout << "Union: " << union1.intUn << std::endl;
     std::cout << "Union: " << union1.charUn << std::endl;
 
-    std::variant<int, bool, char> newApproach; // default initializes 1st element
-    std::cout << "Variant: " << std::get<int>(newApproach) << std::endl;
-    std::cout << "Variant currently holds alternative for char - " << std::holds_alternative<char>(newApproach) << std::endl;
-    // std::cout << "Variant: " << std::get<char>(newApproach) << std::endl;
-    std::cout << "Variant: " << std::get_if<bool>(&newApproach) << std::endl;
-    std::cout << "Variant: " << std::get_if<int>(&newApproach) << std::endl;
+    std::variant<int, bool, char> newApproach;
+    // newApproach = 42;
+    std::cout << "Variant1: " << std::get<int>(newApproach) << std::endl;
+    newApproach = 42;
+    newApproach = true;
+    std::cout << "Variant2: " << std::get<bool>(newApproach) << std::endl;
+    // std::cout << "Variant3: " << std::get<int>(newApproach) << std::endl;
 
+    std::cout << "Variant4: holds currently alternative for char: " << std::holds_alternative<char>(newApproach) << std::endl;
+    std::cout << "Variant4: holds currently alternative for bool: " << std::holds_alternative<bool>(newApproach) << std::endl;
+
+    std::cout << "Variant5: pointer to bool: " << std::get_if<bool>(&newApproach) << std::endl;
+    std::cout << "Variant6: pointer to int: " << std::get_if<int>(&newApproach) << std::endl;
+
+    //----------------------------
     // https://en.cppreference.com/w/cpp/utility/variant
     // https://en.cppreference.com/w/cpp/utility/variant/visit
     // https://www.bfilipek.com/2018/06/variant.html
     // https://www.bfilipek.com/2018/09/visit-variants.html
     // https://www.bfilipek.com/2020/04/variant-virtual-polymorphism.html
     // https://bitbashing.io/std-visit.html
+
     std::variant<Clazz1, Clazz2> clazzesVariant;
 
-    auto visitor = [](auto in) {
-        in(); // in.sayHello()
-    };        // Callable object, functor, lambda,
-    std::visit(visitor, clazzesVariant);
+    auto myVisitor = [](auto input) { input(); };
+    std::visit(myVisitor, clazzesVariant);
     clazzesVariant = Clazz2();
-    std::visit(visitor, clazzesVariant);
+    std::visit(myVisitor, clazzesVariant);
 
-    std::variant<Clazz1, Clazz2, int, std::string> mixedVariant;
-    std::visit(DetermingVisitor{}, mixedVariant);
-    mixedVariant = 42;
-    std::visit(DetermingVisitor{}, mixedVariant);
-    mixedVariant = "Meow"; // or string("Meow")
-    std::visit(DetermingVisitor{}, mixedVariant);
+    //-----------------------------
+    std::variant<Clazz1, Clazz2, Clazz3, int, std::string> anotherClazzesVariant;
+    std::visit(DetermingVisitor(), anotherClazzesVariant);
 
-    /**
-     std::variant<Fluid, LightItem, HeavyItem, FragileItem> package;
+    anotherClazzesVariant = Clazz2();
+    std::visit(DetermingVisitor(), anotherClazzesVariant);
 
-     std::visit(overload{
-            [](Fluid& ) { cout << "fluid\n"; },
-            [](LightItem& ) { cout << "light item\n"; },
-            [](HeavyItem& ) { cout << "heavy item\n"; },
-            [](FragileItem& ) { cout << "fragile\n"; }
-        }, package);
-     *
-     */
-    // Possible uses
-    // Input, where you can get different types (i.e. input process args, ini files, ..)
-    // Returning error result of operation, where you need more than std::optional -> i.e. std::variant<int errorCode, class successResult>
-    // Polymorphism without vtables and inheritance
+    anotherClazzesVariant = Clazz3();
+    std::visit(DetermingVisitor(), anotherClazzesVariant);
+
+    anotherClazzesVariant = 42;
+    std::visit(DetermingVisitor(), anotherClazzesVariant);
+
+    anotherClazzesVariant = "43";
+    std::visit(DetermingVisitor(), anotherClazzesVariant);
+
+    // M.CH. std::overloaded
 
     return 0;
 }
