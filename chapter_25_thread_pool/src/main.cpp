78bb85af23223513376a7ec68718507cc48f5a8f
Zdeno Pavlik <zdeno.pavlik@gmail.com>
2021-07-26 12:35:48 +0200

    wip

--- chapter_25_thread_pool/src/main.cpp
+++ chapter_25_thread_pool/src/main.cpp
@@ -7,6 +7,7 @@
 #include <threadSafeQeueue.hpp>
 
 // https://github.com/ZdenoPavlik/cpp_concurrency_masterclass/blob/master/s8_simple_threadpool/simple_thread_pool.h#L32
+// https://stackoverflow.com/questions/57219650/stdcondition-variablenotify-all-only-wakes-up-one-thread-in-my-threadpool
 
 class thread_pool
 {
@@ -15,7 +16,7 @@ public:
         : done(false)
     {
         auto thread_count = std::thread::hardware_concurrency();
-        std::cout << "Using " << thread_count << " threads" << std::endl;
+        std::cout << std::dec << "Using " << thread_count << " threads" << std::endl;
 
         try
         {
@@ -44,7 +45,7 @@ public:
     void submit(function_type f)
     {
         work_queue.push(std::function<void()>(f));
-        // this->workerCV.notify_one();
+        this->workerCV.notify_one();
     }
 
     bool workQueueEmpty() const
@@ -55,37 +56,53 @@ public:
     void setWorkDone()
     {
         this->done.store(true);
-        // this->workerCV.notify_all();
+        this->workerCV.notify_all();
     }
 
 private:
     std::atomic_bool done; // global command to terminate all threads
     thread_safe_queue<std::function<void()>> work_queue;
     std::vector<std::jthread> threads;
-    // std::condition_variable workerCV;
-    // std::mutex workerM;
+    std::condition_variable workerCV;
+    std::mutex workerM;
 
     static void worker_function(thread_pool* instance)
     {
         while(!instance->done.load())
         {
-            // std::unique_lock<std::mutex> lk(instance->workerM);
-            // instance->workerCV.wait(lk, [&instance] { return !instance->work_queue.empty(); });
+            std::unique_lock<std::mutex> lk(instance->workerM);
+            // std::unique_lock<std::mutex> lk(instance->workerM, std::defer_lock);
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(30));
-
+            // if(lk.try_lock())
+            //{
+            std::cout << "Locked worker" << std::endl;
             if(!instance->work_queue.empty())
             {
+                std::cout << "Queue not empty" << std::endl;
                 auto function = instance->work_queue.pop();
                 function();
             }
+            else
+            {
+                std::cout << "Queue IS empty" << std::endl;
+            }
+            lk.unlock();
+
+            std::cout << "UNlocked worker" << std::endl;
+            //}
+
+            // std::cout << "Locking" << std::endl;
+            instance->workerCV.wait(lk, [&instance] { return !instance->work_queue.empty(); }); // everything locks here...
+            // std::cout << "Unlocking" << std::endl;
+
             // else
             //{
-            //    std::this_thread::yield();
+            //
             //}
 
             // lk.unlock();
-            // instance->workerCV.notify_one();
+            // std::this_thread::yield();
+            // instance->workerCV.notify_all();
         }
     }
 };
